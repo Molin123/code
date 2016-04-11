c608ed52a2a2dcd5fc8cb1e37c949c194c1fc519
molin <molin@molindeMacBook-Pro.local>
2016-04-11 09:40:15 +0800

    1

---
+++ node/cheerioNew.js
@@ -0,0 +1,103 @@
+var http = require('http');
+//var Promise = require('Promise');
+var Promise = require('bluebird');
+var cheerio = require('cheerio');
+//var url = 'http://www.imooc.com/learn/348';
+var baseUrl = 'http://www.imooc.com/learn/';
+var videoIds = [348,259,197,134,75];
+function filterChapters(html){
+    var $=cheerio.load(html);
+
+    var chapters=$(".chapter");
+    var title = $('#main .course-infos .w .hd .l').text();
+    var number = parseInt($($("#main .course-infos .w .statics .static-item ")[2]).children('.meta-value strong').text().trim(),10)
+    var courseData={   	
+    	number:number,
+    	title:title,
+    	videos:[],
+    };
+    chapters.each(function(item){
+        var chapter=$(this);
+        var chapterTitle=chapter.find('strong').text();
+        var videos=chapter.find('.video').children('li');
+        var chapterData={
+            chapterTitle:chapterTitle,
+            videos:[]
+        }
+        videos.each(function(item){
+            var video = $(this).find('.studyvideo');
+            var videoTitle = video.text();
+            var id = video.attr('href').split('video/')[1];
+            chapterData.videos.push({
+                title:videoTitle,
+                id:id
+            });
+        });
+        courseData.videos.push(chapterData);
+
+    });
+    return courseData;
+}
+function printCourseInfo(coursesData){
+	coursesData.forEach(function(courseData){
+		console.log(courseData.number + '人学过' + courseData.title + '\n');
+	})
+	coursesData.forEach(function(courseData){
+		console.log('######' + courseData.title + '\n');
+		courseData.videos.forEach(function(item){
+	        var chapterTitle = item.chapterTitle;
+	        console.log(chapterTitle + '\n');
+	        item.videos.forEach(function(video){
+	            console.log('   【'+video.id+'】'+video.title+'\n');
+	        });
+	    });
+	})
+}
+function getPageAsync(url){
+	return new Promise(function(resolve, reject){
+		console.log('正在爬取' + url);
+
+		http.get(url,function(res){
+		    var html = '';
+		    res.on('data',function(data){
+		        html += data;
+		    });
+		    res.on('end',function(){
+		    	resolve(html)
+		        //var courseData=filterChapters(html);
+		        //console.log(courseData);
+		        //printCourseInfo(courseData);
+		    })
+		}).on('error',function(){
+			reject(e);
+		    console.log('出错了!');
+		});
+	})
+}
+
+var fecthCourseArray = [];
+
+videoIds.forEach(function(id){
+	fecthCourseArray.push(getPageAsync(baseUrl + id));
+});
+
+Promise
+	.all([fecthCourseArray])
+	.then(function(pages){
+		var coursesData = [];
+
+		pages.forEach(function(html){
+			var courses = filterChapters(html);
+			coursesData.push(courses);
+		})
+
+		coursesData.sort(function(a,b){
+			return a.number < b.number
+		})
+
+		printCourseInfo(coursesData);
+
+	})
+
+
+
--- node/readme.md
+++ node/readme.md
@@ -1,3 +1,5 @@
-comment.js   ---- >   利用http module 的request方法给某网站发表评论
-cheerio.js   ---- >   cheerio制作的一个简单爬虫
-ball.js      ---- >   promise回调制作简单动画
+comment.js      ---- >   利用http module 的request方法给某网站发表评论
+cheerio.js      ---- >   cheerio制作的一个简单爬虫
+ball.js         ---- >   promise回调制作简单动画
+https.js        ---- >   构建https服务器
+cheerioNew.js   ---- >   重构爬虫代码
